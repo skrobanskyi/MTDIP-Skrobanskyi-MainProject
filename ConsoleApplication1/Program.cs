184b88b5897e04a364465877790c9759d871291c
Admin-hdd-ПК\Admin-hdd <mr.slaventius1995@gmail.com>
2018-12-12 02:58:58 +0200

    додано рядок private string zmina_v_oryginali_proektu2 = "eee";

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApplication1
         int n; double Ch;
         double[,] matrix;
         double sl = 0;
-        
+        private string zmina_v_oryginali_proektu2 = "eee";
 
         public double this[int i, int j]
         {
