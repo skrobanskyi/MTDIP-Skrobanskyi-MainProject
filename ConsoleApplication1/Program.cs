358f9205ac54a2a0e90e8183065fb6b5bd92b843
Admin-hdd-ПК\Admin-hdd <mr.slaventius1995@gmail.com>
2018-12-12 02:54:03 +0200

    видалено рядки

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -11,11 +11,7 @@ namespace ConsoleApplication1
         int n; double Ch;
         double[,] matrix;
         double sl = 0;
-        private string zmina_v_oryginali_proektu = "eee";
-
-
-
-        private string zmina_v_kopii_proektu = "";
+        
 
         public double this[int i, int j]
         {
